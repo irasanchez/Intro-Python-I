c2ab55ad10d124ebdea08a035ade8ac815865e28
Ira <ira.d.sanchez@gmail.com>
2020-01-17 20:49:50 -0700

    Add completed 11_args.py

--- src/11_args.py
+++ src/11_args.py
@@ -1,10 +1,23 @@
 # Experiment with positional arguments, arbitrary arguments, and keyword
 # arguments.
 
+# positional arguments:
+# "Positional arguments are for those pieces of information that your program absolutely,
+# positively requires to run."
+
+# keyword arguments:
+# allows you to name arguments in a function call so that you can list them in any order
+# function_call(third_arg=3, first_arg=1, second_arg=2) => interpreted as function_call(1, 2, 3)
+
 # Write a function f1 that takes two integer positional arguments and returns
 # the sum. This is what you'd consider to be a regular, normal function.
 
+from functools import reduce
+# need this line ⬆⬆⬆ for reduce method
+
 # YOUR CODE HERE
+def f1(num1, num2):
+    return num1 + num2
 
 print(f1(1, 2))
 
@@ -12,6 +25,11 @@ print(f1(1, 2))
 # sum. Google for "python arbitrary arguments" and look for "*args"
 
 # YOUR CODE HERE
+def f2(*args):
+    sum = 0
+    for num in args:
+        sum += num
+    return sum
 
 print(f2(1))                    # Should print 1
 print(f2(1, 3))                 # Should print 4
@@ -21,13 +39,14 @@ print(f2(7, 9, 1, 3, 4, 9, 0))  # Should print 33
 a = [7, 6, 5, 4]
 
 # What thing do you have to add to make this work?
-print(f2(a))    # Should print 22
+print(f2(*a))    # Should print 22
 
 # Write a function f3 that accepts either one or two arguments. If one argument,
 # it returns that value plus 1. If two arguments, it returns the sum of the
 # arguments. Google "python default arguments" for a hint.
 
 # YOUR CODE HERE
+def f3(num1, num2=1): return num1 + num2
 
 print(f3(1, 2))  # Should print 3
 print(f3(8))     # Should print 9
@@ -42,6 +61,9 @@ print(f3(8))     # Should print 9
 # Google "python keyword arguments".
 
 # YOUR CODE HERE
+def f4(**kwargs):
+    for key, value in kwargs.items():
+        print("key: ", key, "value: ", value)
 
 # Should print
 # key: a, value: 12
@@ -60,4 +82,4 @@ d = {
 }
 
 # What thing do you have to add to make this work?
-f4(d)
+f4(**d)

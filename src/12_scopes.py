0b6f0038ad4b5c89c88bc47aa3e3e09b4cf75ca0
Ira <ira.d.sanchez@gmail.com>
2020-01-19 17:58:03 -0700

    Add PEP8 Edits to 12_scopes.py

--- src/12_scopes.py
+++ src/12_scopes.py
@@ -9,6 +9,7 @@ def changeX():
     global x
     x = 99
 
+
 changeX()
 
 # This prints 12. What do we have to modify in changeX() to get it to print 99?
@@ -29,5 +30,6 @@ def outer():
     # This prints 120. What do we have to change in inner() to get it to print
     # 999? Google "python nested function scope".
     print(y)
+
 
 outer()
